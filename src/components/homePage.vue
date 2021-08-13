e652be904c8f7858dc1a4709bd42a724f1ecc760
Pravinlkr <pravinlakhisarai@gmail.com>
2021-08-13 16:33:51 +0530

    removed comment and extra breakpoints in header

--- src/components/homePage.vue
+++ src/components/homePage.vue
@@ -1,10 +1,10 @@
 <template>
     <v-container class="mt-8">
         <v-row class="text-center">
-            <v-col cols="12" xs="12" sm="5" md="5" lg="4">
+            <v-col cols="12" sm="5" lg="4">
                 <h2>Animals of the Kingdom</h2>
             </v-col>
-            <v-col cols="12" xs="12" sm="7" md="7" lg="8">
+            <v-col cols="12" sm="7" lg="8">
                 <create></create>
             </v-col>
         </v-row>
@@ -25,24 +25,3 @@ export default {
 }
 </script>
 
-<style scoped>
-/*.topHeader{
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
-}
-@media (max-width:500px){
-    .topHeader{
-        flex-direction: column;
-        text-align: center;
-    }
-    .createButton{
-        margin-top: 3%;
-    }
-}
-@media (max-width:1023px){
-    .topHeader{
-        margin:auto 5%;
-    }    
-}*/
-</style>
